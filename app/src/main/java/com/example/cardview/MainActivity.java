83f84757eb008918335d4b027963ca1fc7a99158
Subham-Kumar-Padhy <subhamkumarpadhy14@gmail.com>
2023-11-01 15:25:20 +0530

    Know about CardView

---
+++ app/src/main/java/com/example/cardview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.cardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+//    CardView cardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Implementing attributes of cardView with JAVA
+
+        /*
+
+        cardView = findViewById(R.id.cardView);
+
+         cardView.setRadius(5.0f);
+         cardView.setElevation(11.0f);
+         cardView.setUseCompatPadding(true);
+
+         */
+
+    }
+}
